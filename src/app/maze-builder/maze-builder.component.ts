83f18369538fbee185ac3c6e91092479423311c7
Aaron Rodriguez <adashrod@gmail.com>
2018-04-22 21:25:24 -0400

    - live update of maze visual when rows, cols, or seed change - addition of regenerate button to manually rebuild the maze - organized UI into a proper layout - separate buildMaze and exportSvg into separate functions to allow live maze updating without sacrificing performance - download button for easy downloading of generated SVG file

--- src/app/maze-builder/maze-builder.component.ts
+++ src/app/maze-builder/maze-builder.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import Big from "big.js";
+import { saveAs } from "file-saver";
 
 import KruskalsAlgorithm from "app/algorithms/kruskals-algorithm";
 import MazeGenerator from "app/algorithms/maze-generator";
@@ -9,6 +10,7 @@ import Unit from "app/common/unit";
 import LinearWallModelGenerator from "app/factories/linear-wall-model-generator";
 import RectangularWallModelGenerator from "app/factories/rectangular-wall-model-generator";
 import SheetWallModelGenerator from "app/factories/sheet-wall-model-generator";
+import { min } from "app/misc/big-util";
 import CalibrationRectangle from "app/models/calibration-rectangle";
 import Maze from "app/models/maze";
 import MazeConfig from "app/models/maze-config";
@@ -23,57 +25,82 @@ export class MazeBuilderComponent implements OnInit {
     readonly mazeUnits: Unit[] = [Unit.INCHES, Unit.CENTIMETERS, Unit.MILLIMETERS];
     readonly rectangleUnits: Unit[] = [Unit.INCHES, Unit.CENTIMETERS];
 
+    mazeConfig: MazeConfig = new MazeConfig();
+    randomSeed: string = "";
+    lastSeedUsed: string;
+
     maxWidth: number = 19.5;
     maxHeight: number = 11;
-    mazeConfig: MazeConfig = new MazeConfig();
+    maxPrinterUnits = Unit.INCHES;
+
     includeCalibrationRectangle: boolean = false;
     calibrationRectangleConfig: CalibrationRectangle = new CalibrationRectangle();
-    randomSeed: string = "";
     algorithms: MazeGenerator[] = [new PrimsAlgorithm(), new KruskalsAlgorithm];
     currentAlgorithm = 0;
 
-    testMaze: Maze;
-    testSvgSrc: string;
-    svgDataUrl: SafeUrl;
+    maze: Maze;
+    svgSrc: string | null;
+    svgDataUrl: SafeUrl | null;
+    showSvgPreview: boolean = false;
 
     constructor(private sanitizer: DomSanitizer) {}
 
-    ngOnInit() {}
+    ngOnInit(): void {
+        this.mazeConfig.addChangeListener((oldVal, newVal) => {
+            this.buildMaze();
+        });
+    }
 
     buildMaze() {
+        if (typeof this.mazeConfig.numRows !== "number" || typeof this.mazeConfig.numCols !== "number") {
+            return;
+        }
+        this.svgSrc = null;
+        this.svgDataUrl = null;
         const start = new Date().getTime();
-        console.log(`building a maze with mazeConfig: ${JSON.stringify(this.mazeConfig)}, maxWidth: ${this.maxWidth}, maxHeight: ` +
-            `${this.maxHeight}, ${this.includeCalibrationRectangle ? "include" : "no"} calibration rectangle ` +
-            `${this.includeCalibrationRectangle ? JSON.stringify(this.calibrationRectangleConfig) : ""}`);
         const maze = new Maze(this.mazeConfig.numCols, this.mazeConfig.numRows);
-        const a = this.algorithms[this.currentAlgorithm];
-        a.seed = this.randomSeed.toString().length > 0 ? this.randomSeed : new Date().getTime();
-        maze.build(a);
-        console.log(`seed used: ${a.seed}`);
-        this.testMaze = maze;
+        const algo = this.algorithms[this.currentAlgorithm];
+        algo.seed = this.randomSeed.toString().length > 0 ? this.randomSeed : new Date().getTime();
+        this.lastSeedUsed = algo.seed.toString();
+        maze.build(algo);
+        console.log(`seed used: ${algo.seed}`);
+        this.maze = maze;
+        console.log(`maze build time: ${new Date().getTime() - start} ms`);
+    }
 
-        const linearWallModelGenerator = new LinearWallModelGenerator(maze);
+    exportSvg() {
+        const start = new Date().getTime();
+        const linearWallModelGenerator = new LinearWallModelGenerator(this.maze);
         const linearWallModel = linearWallModelGenerator.generate();
         const rectangularWallModelGenerator = new RectangularWallModelGenerator(linearWallModel);
         const rectangularWallModel = rectangularWallModelGenerator.generate();
         const sheetWallModelGenerator = new SheetWallModelGenerator(rectangularWallModel);
         sheetWallModelGenerator.hallWidth = new Big(this.mazeConfig.hallWidth).mul(this.mazeConfig.unit.pixelsPer);
         sheetWallModelGenerator.materialThickness = new Big(this.mazeConfig.materialThickness).mul(this.mazeConfig.unit.pixelsPer);
-        sheetWallModelGenerator.maxHeight = new Big(this.maxHeight).mul(this.mazeConfig.unit.pixelsPer);
-        sheetWallModelGenerator.maxWidth = new Big(this.maxWidth).mul(this.mazeConfig.unit.pixelsPer);
-        sheetWallModelGenerator.notchHeight = new Big(this.mazeConfig.notchHeight).mul(this.mazeConfig.unit.pixelsPer);
+        sheetWallModelGenerator.maxHeight = new Big(this.maxHeight).mul(this.maxPrinterUnits.pixelsPer);
+        sheetWallModelGenerator.maxWidth = new Big(this.maxWidth).mul(this.maxPrinterUnits.pixelsPer);
+        sheetWallModelGenerator.notchHeight = min(Unit.MILLIMETERS.pixelsPer.mul("4"),
+            new Big(this.mazeConfig.hallWidth).mul(this.mazeConfig.unit.pixelsPer).mul(".33"));
         sheetWallModelGenerator.separationSpace = new Big(this.mazeConfig.separationSpace).mul(this.mazeConfig.unit.pixelsPer);
         sheetWallModelGenerator.wallHeight = new Big(this.mazeConfig.wallHeight).mul(this.mazeConfig.unit.pixelsPer);
         const sheetWallModel = sheetWallModelGenerator.generate();
-        const mazePrinter = new MazePrinter(sheetWallModel, new Big(this.maxWidth), new Big(this.maxHeight));
-        let svgSrc;
+        const mazePrinter = new MazePrinter(sheetWallModel, new Big(this.maxWidth).mul(this.maxPrinterUnits.pixelsPer),
+            new Big(this.maxHeight).mul(this.maxPrinterUnits.pixelsPer));
         if (this.includeCalibrationRectangle) {
-            svgSrc = mazePrinter.printSvg(this.calibrationRectangleConfig);
+            this.svgSrc = mazePrinter.printSvg(this.calibrationRectangleConfig);
         } else {
-            svgSrc = mazePrinter.printSvg();
+            this.svgSrc = mazePrinter.printSvg();
         }
-        this.testSvgSrc = svgSrc;
-        this.svgDataUrl = this.sanitizer.bypassSecurityTrustUrl(`data:image/svg+xml;utf-8,${svgSrc}`);
-        console.log(`total time: ${new Date().getTime() - start} ms`);
+        this.svgDataUrl = this.sanitizer.bypassSecurityTrustUrl(`data:image/svg+xml;utf-8,${this.svgSrc}`);
+        console.info(`svg export time: ${new Date().getTime() - start} ms`);
+    }
+
+    toggleSvgPreview() {
+        this.showSvgPreview = !this.showSvgPreview;
+    }
+
+    downloadSvg() {
+        const file = new File([this.svgSrc || ""], "maze.svg", {type: "image/svg+xml;charset=utf-8"});
+        saveAs(file);
     }
 }
--- src/app/models/maze-config.ts
+++ src/app/models/maze-config.ts
@@ -5,13 +5,45 @@ import Unit from "app/common/unit";
  * a class for encapsulating all of the config needed for building a maze
  */
 export default class MazeConfig {
-    numRows: number = 8;
-    numCols: number = 8;
+    _numRows: number;
+    _numCols: number;
     unit: Unit = Unit.INCHES;
     wallHeight: number = .5;
     materialThickness: number = .118;
     hallWidth: number = .5;
-    notchHeight: number = .2;
     separationSpace: number = .05;
     algorithm: MazeGenerator;
+    private listeners: ((oldVal: any, newVal: any) => void)[] = [];
+
+    get numRows(): number {
+        return this._numRows;
+    }
+
+    set numRows(newVal: number) {
+        const oldVal = this.numRows;
+        this._numRows = newVal;
+        if (oldVal !== newVal) {
+            for (const l of this.listeners) {
+                l(oldVal, newVal);
+            }
+        }
+    }
+
+    get numCols(): number {
+        return this._numCols;
+    }
+
+    set numCols(newVal: number) {
+        const oldVal = this.numCols;
+        this._numCols = newVal;
+        if (oldVal !== newVal) {
+            for (const l of this.listeners) {
+                l(oldVal, newVal);
+            }
+        }
+    }
+
+    addChangeListener(listener: (oldVal: any, newVal: any) => void): void {
+        this.listeners.push(listener);
+    }
 }
