59f1753d05220fd61e5d8a685dfb99f0f78af4ae
Aaron Rodriguez <adashrod@gmail.com>
2018-05-11 16:12:46 -0400

    moving lightbox dimension calculations to after onload of images since it wasn't actually doing anything useful before image load

--- src/app/lightbox-thumbnail/lightbox-thumbnail.component.ts
+++ src/app/lightbox-thumbnail/lightbox-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { AfterViewInit, Component, ElementRef, OnInit, Input, ViewChild } from "
     templateUrl: "./lightbox-thumbnail.component.html",
     styleUrls: ["./lightbox-thumbnail.component.css"]
 })
-export class LightboxThumbnailComponent implements AfterViewInit, OnInit {
+export class LightboxThumbnailComponent implements OnInit {
     @Input() title: string;
     @Input() image: string;
     @Input() thumbnailWidth: number;
@@ -15,17 +15,17 @@ export class LightboxThumbnailComponent implements AfterViewInit, OnInit {
 
     constructor() {}
 
-    ngOnInit() {}
-
-    ngAfterViewInit() {
-        const aspectRatio = this.thumbnail.nativeElement.width / this.thumbnail.nativeElement.height;
-        if (this.thumbnailWidth) {
-            this.thumbnail.nativeElement.setAttribute("width", this.thumbnailWidth);
-            this.thumbnail.nativeElement.setAttribute("height", this.thumbnailWidth / aspectRatio);
-        } else if (this.thumbnailHeight) {
-            this.thumbnail.nativeElement.setAttribute("width", this.thumbnailHeight * aspectRatio);
-            this.thumbnail.nativeElement.setAttribute("height", this.thumbnailHeight);
-        }
+    ngOnInit() {
+        this.thumbnail.nativeElement.onload = () => {
+            const aspectRatio = this.thumbnail.nativeElement.width / this.thumbnail.nativeElement.height;
+            if (this.thumbnailWidth) {
+                this.thumbnail.nativeElement.setAttribute("width", this.thumbnailWidth);
+                this.thumbnail.nativeElement.setAttribute("height", Math.round(this.thumbnailWidth / aspectRatio));
+            } else if (this.thumbnailHeight) {
+                this.thumbnail.nativeElement.setAttribute("width", Math.round(this.thumbnailHeight * aspectRatio));
+                this.thumbnail.nativeElement.setAttribute("height", this.thumbnailHeight);
+            }
+        };
     }
 
     showLightbox() {
