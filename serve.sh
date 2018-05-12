b0dcddf2217377e7cae277a2da9a1516f37c5523
Aaron Rodriguez <adashrod@gmail.com>
2018-05-12 12:23:34 -0400

    added some meta tags to index.html and fixed serve script to work across the LAN

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ng serve --live-reload=false --host=0.0.0.0 --serve-path=LaserCutMazes --deploy-url=http://localhost:4200/LaserCutMazes
+ng serve --live-reload=false --host=0.0.0.0 --serve-path=LaserCutMazes --deploy-url=/LaserCutMazes
